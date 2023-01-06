88c3742d6c9a431851007b546329467ce89171c5
Kacper <kacper.gajko1@gmail.com>
2023-01-06 12:02:23 +0100

    Fixed character shaking when camera moves

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -10,7 +10,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, zOffset);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
